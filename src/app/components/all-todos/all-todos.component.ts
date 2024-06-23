3ea13306f1c1fd4d66789bf50c8f17470a1cc4bb
Lukas Nolting <lukas.nolting@icloud.com>
2024-06-23 14:45:10 +0200

    show todos from backend

--- src/app/components/all-todos/all-todos.component.ts
+++ src/app/components/all-todos/all-todos.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { lastValueFrom } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-all-todos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './all-todos.component.html',
   styleUrl: './all-todos.component.scss'
 })
-export class AllTodosComponent {
+export class AllTodosComponent implements OnInit {
+  
+  todos:any = [];
+  error: string = '';
+  
+  constructor(private http: HttpClient) { }
+  
+  
+  async ngOnInit() {
+    try{
+    this.todos = await this.loadTodos();
+    console.log(this.todos);  
+    }catch(e){
+      this.error = 'Could not load todos';
+      
+    }  
+  }
+
+  loadTodos() {
+    const url =  environment.baseUrl + '/todos/';
+    let headers = new HttpHeaders();
+    headers = headers.append('Authorization', 'Token ' + localStorage.getItem('token'));
+    return lastValueFrom(this.http.get(url, {headers: headers}));
+  }
 
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -13,9 +16,17 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
 
+  constructor(private as:AuthService, private router:Router) {}
 
-  login() {
-    console.log(this.username, this.password);
-  };
-  
+  async login() { 
+    try{
+      let resp = await this.as.loginWithUsernameAndPassword(this.username, this.password);
+      console.log(resp);
+      localStorage.setItem('token', (resp as any).token);
+      this.router.navigate(['/todos']);
+    }catch(e){
+      alert('Login failed');
+      console.error(e);
+    }
+      };
 }
